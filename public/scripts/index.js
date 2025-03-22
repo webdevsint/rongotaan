3a2ff2aacb8bed2557672b96a9fe4590058b607c
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2025-03-22 14:31:49 +0600

    IOS bug fix

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,20 @@
-const video = document.querySelector('video');
-const muteButton = document.querySelector('.video-overlay');
+const video = document.querySelector("video");
+const muteButton = document.querySelector(".video-overlay");
 
-muteButton.addEventListener('click', () => {
+function isIOS() {
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  }
+
+if (!isIOS()) {
+  video.removeAttribute("controls");
+
+  muteButton.addEventListener("click", () => {
     if (video.muted) {
-        video.muted = false;
-        muteButton.innerHTML = '<i class="fa-solid fa-volume"></i>'
+      video.muted = false;
+      muteButton.innerHTML = '<i class="fa-solid fa-volume"></i>';
     } else {
-        video.muted = true;
-        muteButton.innerHTML = '<i class="fa-solid fa-volume-slash"></i>'
+      video.muted = true;
+      muteButton.innerHTML = '<i class="fa-solid fa-volume-slash"></i>';
     }
-});
+  });
+}
